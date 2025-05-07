bb52e211939cb0da04553040b2218b6b460f3c37
Arbaz Khan <khanarbazmlk15@gmail.com>
2025-05-07 15:54:41 +0530

    email and pass validation added

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -11,13 +11,29 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  var emailInput = '';
+  var passInput = '';
+  final _form = GlobalKey<FormState>();
+
+  var _isLogin = true;
+
+  void _submit() {
+    final isValid = _form.currentState!.validate();
+
+    if (isValid) {
+      _form.currentState!.save();
+      print(emailInput);
+      print(passInput);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.primary,
-      appBar: AppBar(
-        title: const Text('Registration'),
-      ),
+      // appBar: AppBar(
+      //   title: const Text('Registration'),
+      // ),
       body: Center(
         child: SingleChildScrollView(
           child: Column(
@@ -34,30 +50,70 @@ class _AuthScreenState extends State<AuthScreen> {
                 child: Image.asset('assets/images/chat.png'),
               ),
               Card(
-                margin: EdgeInsets.all(20),
+                margin: const EdgeInsets.all(20),
                 child: SingleChildScrollView(
                   child: Padding(
-                    padding: EdgeInsets.all(16),
+                    padding: const EdgeInsets.all(16),
                     child: Form(
+                        key: _form,
                         child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Email Address',
-                          ),
-                          keyboardType: TextInputType.emailAddress,
-                          autocorrect: false,
-                          textCapitalization: TextCapitalization.none,
-                        ),
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            labelText: 'Password ',
-                          ),
-                          obscureText: true, //hide the character
-                        ),
-                      ],
-                    )),
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'Email Address',
+                              ),
+                              keyboardType: TextInputType.emailAddress,
+                              autocorrect: false,
+                              textCapitalization: TextCapitalization.none,
+                              validator: (value) {
+                                if (value == null ||
+                                    value.trim().isEmpty ||
+                                    !value.contains('@')) {
+                                  return 'Please enter valid email address';
+                                }
+                                return null;
+                              },
+                              onSaved: (newValue) => emailInput = newValue!,
+                            ),
+                            TextFormField(
+                              decoration: const InputDecoration(
+                                labelText: 'Password ',
+                              ),
+                              validator: (value) {
+                                if (value == null ||
+                                    value.trim().isEmpty ||
+                                    value.length < 6) {
+                                  return 'Password must be atleast 6 character long.';
+                                }
+                                return null;
+                              },
+                              obscureText: true, //hide the character
+                              onSaved: (newValue) => passInput = newValue!,
+                            ),
+                            const SizedBox(
+                              height: 12,
+                            ),
+                            ElevatedButton(
+                              onPressed: _submit,
+                              child: Text(_isLogin ? 'Log in' : 'Sign Up'),
+                              style: ElevatedButton.styleFrom(
+                                  backgroundColor: Theme.of(context)
+                                      .colorScheme
+                                      .primaryContainer),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isLogin = !_isLogin;
+                                });
+                              },
+                              child: Text(_isLogin
+                                  ? 'Create an account '
+                                  : 'Already have an account'),
+                            ),
+                          ],
+                        )),
                   ),
                 ),
               ),
